ae66f1e2a5ee64bb08ae236c1444ca491f471a5d
nsutic <nikolasutic1@gmail.com>
2019-04-25 23:20:34 +0200

    Added database scraper.

---
+++ Scraper/format.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp "$1" backup_"$1"
+sed -i -E 's/\s[0-9]+[a-z]+//' "$1"
+vim -nEs +'%s/č/c/g' +wq "$1"
+vim -nEs +'%s/ć/c/g' +wq "$1"
+vim -nEs +'%s/š/s/g' +wq "$1"
+vim -nEs +'%s/ž/z/g' +wq "$1"
+vim -nEs +'%s/đ/dj/g' +wq "$1"
---
+++ Scraper/scraper.py
@@ -0,0 +1,68 @@
+# https://cenoteka.rs/kategorija/namirnice
+# https://cenoteka.rs/kategorija/mlecni-proizvodi
+# https://cenoteka.rs/kategorija/voce-i-povrce
+# https://cenoteka.rs/kategorija/meso-i-riba
+
+from selenium import webdriver
+import sys
+import re
+from bs4 import BeautifulSoup
+
+def format(mera):
+	mera=mera.split()
+	for char in mera:
+		if mera == ' ':
+			del mera
+	return ''.join(mera)
+
+# Incijalizacija selenium kontrole pretrazivaca
+browser = webdriver.Firefox()
+
+# Kategorije za prikupljanje cena
+kategorije = ["https://cenoteka.rs/kategorija/namirnice", "https://cenoteka.rs/kategorija/mlecni-proizvodi", "https://cenoteka.rs/kategorija/voce-i-povrce", "https://cenoteka.rs/kategorija/meso-i-riba"]
+
+# Nalazenje linkova svih namirnica
+namirnice = []
+for kategorija in kategorije:
+	browser.get(kategorija)
+	bsObj = BeautifulSoup(browser.page_source, "lxml")
+	for namirnica in bsObj.findAll("div",{"class":re.compile("col-6")}):
+		namirnica=namirnica.find("a",href = re.compile("proizvodi"))
+		if namirnica is not None:
+			namirnice.append(namirnica)
+
+prodavnice = ["IDEA", "Gomex", "Maxi", "Univerexport", "Tempo", "DIS", "Roda","LIDL"]
+
+# Izvlacenje informacija za artikle
+baza = open("baza_cena.txt", "w+")
+for namirnica in namirnice:
+	stranica = 1
+	# Menjanje stranica
+	browser.get("https://cenoteka.rs" + namirnica["href"] + "?page=" + str(stranica))
+	bsObj = BeautifulSoup(browser.page_source, "lxml")
+	while bsObj.find("div", {"class":re.compile("alert-danger")}) is None:
+		browser.get("https://cenoteka.rs" + namirnica["href"] + "?page=" + str(stranica))
+		bsObj = BeautifulSoup(browser.page_source, "lxml")
+		for produkt in bsObj.findAll("div", {"data-product-id":re.compile("\d+")}):
+			try:
+				ime = produkt.find("a", href = re.compile("artikal"), text=re.compile("\w+")).get_text()
+			except AttributeError:
+				continue
+			mera = produkt.find("div", {"class":re.compile("text-center")}).get_text()
+			mera=format(mera)
+			mincena = 10000000
+			prodavnica = 0
+			minprodavnica = 0
+			for cena in produkt.findAll("div", {"class":re.compile("^price")}):
+				cena = re.findall("\d+,?\d*", cena.get_text())
+				if len(cena) != 0:
+					cena = int(float(cena[0].replace(",", ".")))
+					if cena < mincena:
+						minprodavnica = prodavnica
+						mincena = cena
+				prodavnica+= 1
+			baza.write(ime.lower() + "," + mera.lower() + "," + str(mincena).lower() + "," + prodavnice[minprodavnica].lower() + "\r\n")
+		stranica+= 1
+
+baza.close()
+browser.quit()
