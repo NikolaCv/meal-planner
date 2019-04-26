08f08f7f91915a2c3d677ab1c4655582104c2468
nsutic <nikolasutic1@gmail.com>
2019-04-26 21:42:57 +0200

    Changes to algos fils.

--- Meal Planner/nsutic_algos.cpp
+++ Meal Planner/nsutic_algos.cpp
@@ -1,21 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define start_size 100
+#define max_recipe_len 100
 
-typedef char[100] recipe_name;
+//typedef char[max_recipe_len] recipe_name;
 
-void* expand(void *ptr,size_t prevsize) // Function to realloc
+void error()
 {
-	return realloc(ptr,prevsize*2);
+	fprintf(stderr,"-1");
+	exit(EXIT_FAILURE);
 }
 
-typedef struct recipe_nv // Recipe Name/Value
+void* expand(void *ptr,size_t prev_size) // Function to realloc
 {
-	char name[100];
-	float cena;
-};
+	return realloc(ptr,prev_size*2);
+}
 
-char **by_budget(FILE *input,int budget)
+typedef struct // Recipe Name/Value
 {
-	recipe_nv *database=(recipe_nv*)malloc(start_size*sizeof(
+	char name[max_recipe_len];
+	float price;
+}recipe_nv;
+
+void by_budget(FILE *input_stream,int budget)
+{
+	/* Read recipes and prices into database, then perform an algorithm 
+	and print a monthly meal plan sticking with the budget.*/
+	recipe_nv *database=(recipe_nv*)malloc(start_size*sizeof(recipe_nv));
+	FILE *stream=input_stream;
+	int prev_size=start_size; // Saving the previous array size for expansion.
+	int i=0; // Database index counter, checking for illegal index
+	while(fscanf(stream,"%s %f",database[i].name,&database[i++].price)!=EOF){
+		if(i==prev_size){
+			database=(recipe_nv*)expand(database,prev_size);
+			prev_size*=2;		
+		}
+	}
+	for(int j=0;j<i;j++)
+		printf("%s %f\n",database[j].name,database[j].price);
+	/* --------Not finished-------- */
+}
+
+/* -----------Test main function----------- */
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2) 
+		error();
+	FILE *in=fopen(argv[1],"r");
+	if(in==NULL)
+		error();
+	by_budget(in,10000);
+	return 0;
 }
